22e014ba9ce9796549514ded53d1d17abab00421
IsmaelMA97 <fernandoismael1997@gmail.com>
2024-12-17 19:07:52 +0100

    cosas de clase

--- Engine/SceneManager/SceneManager.cs
+++ Engine/SceneManager/SceneManager.cs
@@ -111,7 +111,6 @@ namespace GameEngine
             FollowCamera followC;
             DirectionalLight directionalLightC;
             GameObject sirMartinO;
-            Lizzard lizzardC;
             GameObject lizzardO;
             BoxCollider boxC;
             SphereCollider sphereC;
@@ -131,222 +130,69 @@ namespace GameEngine
 
             scene.AddGameObject(go);
 
+            string[] props = {"", };
+
+            string[] propsNames = {"CoverLane", };
+
 
             go = new GameObject();
-            go.name = "SirMartin";
-            go.AddComponent(new Transform());
-
-            c = new SirMartin();
-            go.AddComponent(c);
-
-            rendererC = new Renderer();
-            rendererC.modelId = "Model2.obj";
-            rendererC.shaderId = "Default.shader";
-            rendererC.textureId = "Texture2.png";
-
-            go.AddComponent(rendererC);
-
-            boxC = new BoxCollider();
-            boxC.size = new Vector3(2, 1, 2);
-
-            go.AddComponent(boxC);
-
-            triggerC = new Trigger();
-
-            go.AddComponent(triggerC);
-
-            sirMartinO = go;
-            scene.AddGameObject(go);
-
-            go = new GameObject();
-            go.name = "Box";
+            go.name = propsNames[i];
             go.AddComponent(new Transform());
 
             rendererC = new Renderer();
-            rendererC.modelId = "Model1.obj";
+            rendererC.modelId = props[i];
             rendererC.shaderId = "Default.shader";
             rendererC.textureId = "Texture1.png";
 
             go.AddComponent(rendererC);
-
-
             scene.AddGameObject(go);
 
             go = new GameObject();
-            go.name = "Terrain";
+            go.name = "GroundCollider";
             go.AddComponent(new Transform());
-
-            rendererC = new Renderer();
-            rendererC.modelId = "Terrain.obj";
-            rendererC.shaderId = "Default.shader";
-            rendererC.textureId = "Grass.png";
-
-            go.AddComponent(rendererC);
-
-            scene.AddGameObject(go);
-
-            go = new GameObject();
-            go.name = "Lizzard";
-            go.AddComponent(new Transform());
-
-            go.transform.position = new Vector3(10, 0, 0);
-
+            go.@static = true;
             boxC = new BoxCollider();
-            boxC.size = new Vector3(2, 1, 2);
+            boxC.size = new Vector3(100,0.2f,100);
 
             go.AddComponent(boxC);
 
             rigidC = new Rigidbody();
             rigidC.isKinematic = true;
-
+            rigidC.mass = 1;
             go.AddComponent(rigidC);
 
-            lizzardC = new Lizzard();
+            scene.AddGameObject(go);
 
-            go.AddComponent(lizzardC);
+            go = new GameObject();
+            go.name = "HandRight";
+            go.AddComponent(new Transform());
 
             rendererC = new Renderer();
-            rendererC.modelId = "Lizzard.obj";
+            rendererC.modelId = "HandleRight.obj";
             rendererC.shaderId = "Default.shader";
-            rendererC.textureId = "Blue.png";
-
+            rendererC.textureId = "Texture1.png";
             go.AddComponent(rendererC);
 
-            lizzardO = go;
+            boxC = new BoxCollider();
+            boxC.size = new Vector3(0.5f, 0.01f, 0.12f);
+            go.AddComponent(boxC);
 
-            scene.AddGameObject(go);
+            go.AddComponent(new Trigger());
+
+
+
+
+
+
 
             go = new GameObject();
             go.name = "MainCamera";
             go.AddComponent(new Transform());
 
+            go.transform.position = new Vector3(0, 0.6f, 2);
+            go.transform.rotation = new Vector3(-20, 0, 0);
             cameraC = new Camera();
             go.AddComponent(cameraC);
-
-            followC = new FollowCamera();
-
-            followC.target = lizzardO.transform;
-
-            go.AddComponent(followC);
-
-            scene.AddGameObject(go);
-
-            Random r = new Random(0);
-
-            for(int i = 0; i < 300; i ++)
-            {
-
-                go = new GameObject();
-                go.name = String.Format("PhysicBox{0:000}", i);
-                go.AddComponent(new Transform());
-
-                go.transform.position = new Vector3(r.NextSingle() * 12 - 12.5f, r.NextSingle() * 50, r.NextSingle() * 25 - 12.5f);
-                go.transform.rotation = new Vector3(r.NextSingle() * 90, r.NextSingle() * 90, r.NextSingle() * 90);
-                go.transform.scale = new Vector3(0.5f + r.NextSingle() * 0.5f, 0.5f + r.NextSingle() * 0.5f, 0.5f + r.NextSingle() * 0.5f);
-
-                boxC = new BoxCollider();
-                boxC.size = new Vector3(1, 1, 1);
-
-                go.AddComponent(boxC);
-
-                rigidC = new Rigidbody();
-                rigidC.isKinematic = false;
-
-                rigidC.speed = new Vector3(r.NextSingle() * 25 - 12.5f, r.NextSingle() * 25 - 12.5f, r.NextSingle() * 25 - 12.5f);
-                rigidC.angularSpeed = new Vector3(r.NextSingle() * 10 - 5, r.NextSingle() * 10 - 5, r.NextSingle() * 10 - 5);
-
-                rigidC.mass = boxC.size.X * boxC.size.Y * boxC.size.Z * 10;
-
-                go.AddComponent(rigidC);
-
-                rendererC = new Renderer();
-                rendererC.modelId = "UnitBox.obj";
-                rendererC.shaderId = "Default.shader";
-                rendererC.textureId = "Wood.png";
-
-                go.AddComponent(rendererC);
-
-
-                scene.AddGameObject(go);
-
-
-                go = new GameObject();
-                go.name = String.Format("PhysicSphere{0:000}", i);
-                go.AddComponent(new Transform());
-
-                go.transform.position = new Vector3(r.NextSingle() * 50 - 25, r.NextSingle() * 50, r.NextSingle() * 50 - 25);
-                go.transform.rotation = new Vector3(0, 0, 0);
-                go.transform.scale = new Vector3(1, 1, 1) * (0.5f + r.NextSingle() * 0.5f);
-
-                sphereC = new SphereCollider();
-                sphereC.radius = 0.5f;
-
-                go.AddComponent(sphereC);
-
-                rigidC = new Rigidbody();
-                rigidC.isKinematic = false;
-                rigidC.mass = 4 / 3 * MathF.PI * MathF.Pow(sphereC.radius, 3) * 10;
-
-                rigidC.speed = new Vector3(r.NextSingle() * 25 - 12.5f, r.NextSingle() * 25 - 12.5f, r.NextSingle() * 25 - 12.5f);
-                rigidC.angularSpeed = new Vector3(r.NextSingle() * 10 - 5, r.NextSingle() * 10 - 5, r.NextSingle() * 10 - 5);
-
-                go.AddComponent(rigidC);
-
-                rendererC = new Renderer();
-                rendererC.modelId = "UnitSphere.obj";
-                rendererC.shaderId = "Default.shader";
-                rendererC.textureId = "Wood.png";
-
-                go.AddComponent(rendererC);
-
-                scene.AddGameObject(go);
-
-            }
-
-            go = new GameObject();
-            go.name = "Trigger";
-            go.AddComponent(new Transform());
-
-            go.transform.position = new Vector3(3, 10, 0);
-            go.transform.rotation = new Vector3(0, 0, 0);
-            go.transform.scale = new Vector3(1, 1, 1) * 8;
-
-            sphereC = new SphereCollider();
-            sphereC.radius = 0.5f;
-
-            triggerC = new Trigger();
-            go.AddComponent(triggerC);
-
-            go.AddComponent(sphereC);
-
-            rendererC = new Renderer();
-            rendererC.modelId = "UnitSphere.obj";
-            rendererC.shaderId = "Default.shader";
-            rendererC.textureId = "Wood.png";
-
-            go.AddComponent(rendererC);
-
-
-            scene.AddGameObject(go);
-
-
-            go = new GameObject();
-            go.name = "Floor";
-            go.@static = true;
-            go.AddComponent(new Transform());
-
-            boxC = new BoxCollider();
-            boxC.size = new Vector3(500, 1, 500);
-
-            go.AddComponent(boxC);
-
-            rigidC = new Rigidbody();
-
-            go.AddComponent(rigidC);
-
-            go.transform.position = new Vector3(0, -0.5f, 0);
-            go.transform.rotation = new Vector3(0, 0, 0);
-
             scene.AddGameObject(go);
             
             return scene;
---
+++ Game/Hand.cs
@@ -0,0 +1,138 @@
+﻿using Silk.NET.GLFW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    internal class Hand : Component
+    {
+        enum State
+        {
+            idle, 
+            grabbing
+        }
+
+
+        Rigidbody grabbed;
+
+        State state;
+        State nextstate;
+
+        Vector2 previousMousePosition;
+        public override void Start()
+        {
+            grabbed = null;
+            state = State.idle;
+            nextstate = State.idle;
+
+            previousMousePosition = Input.GetMousePosition();
+        }
+
+        public override void Update(float deltaTime)
+        {
+            Vector2 mouseposition = Input.GetMousePosition();
+            Vector2 deltaMousePosition = mouseposition - previousMousePosition;
+
+            if (Input.IsMouseButtonPressed(1))
+            {
+                gameObject.transform.position += 0.02f * new Vector3(deltaMousePosition.X,deltaMousePosition.Y, 0);
+            }
+            else
+            {
+                gameObject.transform.position += 0.02f * new Vector3(deltaMousePosition.X, 0, deltaMousePosition.Y);
+            }
+            previousMousePosition = mouseposition;
+
+            Vector3 position = gameObject.transform.position;
+
+            if (position.X < -3.0f)
+            {
+                position.X = -2.0f;
+            }else if(position.X > 2.0f)
+            {
+                position.X = 2.0f;
+            }
+
+            if (position.Y < 0f)
+            {
+                position.Y = 0.0f;
+            }
+            else if (position.Y > 1.0f)
+            {
+                position.Y = 1.0f;
+            }
+
+
+
+            if (position.Z < -0.5f)
+            {
+                position.Z = -0.5f;
+            }
+            else if (position.Z > 2.5f)
+            {
+                position.Z = 2.5f;
+            }
+
+            gameObject.transform.position = position;
+
+
+
+            if (state == State.idle) 
+            {
+                //comportamiento normal cuando no tengo objeto
+
+
+            }
+            else // state == State.grabbing
+            {
+                //Comportamiento normal tengo objeto
+                Transform t = grabbed.GetGameObject().transform;
+                t.position = gameObject.transform.position;
+                t.rotation = gameObject.transform.rotation;
+
+                //Tengo que cambiar de estado?
+                if (!Input.IsMouseButtonPressed(0))
+                {
+                    nextstate = State.idle;
+                }
+            }
+
+            //Cambios de estado
+            if (state != nextstate)
+            {
+                if (nextstate == State.grabbing)
+                {
+                    //Comportamiento al entrar en grabbing
+                    grabbed.isKinematic = true;
+
+                }
+                else //nextState == State.idle
+                {
+                    //Comportamiento al entrar en idle
+
+                }
+
+
+                state = nextstate;
+            }
+        }
+
+
+        public override void OnTriggerEnter(Rigidbody other)
+        {
+            if (state == State.idle && Input.IsMouseButtonPressed(0) && other.GetGameObject().name == "BowlingBall")
+            {
+                grabbed = other;
+                nextstate= State.grabbing;
+            }
+
+
+        }
+
+
+    }
+}
