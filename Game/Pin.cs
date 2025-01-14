5a2480c20a8dd60606370d73032ff0b23740b7c7
IsmaelMA97 <fernandoismael1997@gmail.com>
2025-01-14 18:59:41 +0100

    Update Pin.cs

--- Game/Pin.cs
+++ Game/Pin.cs
@@ -46,7 +46,7 @@ namespace GameEngine
                     rb.isKinematic = false;
 
                     Vector3 collisionForce = collision.rigidbody.speed * rb.mass * 0.8f;
-                    rb.AddForce(collisionForce, Physics.ForceMode.impulse);
+                    rb.AddForce(collisionForce, Physics.ForceMode.acceleration);
 
                     if (!isFallen)
                     {
